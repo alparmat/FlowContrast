4b4f1b39e14612275e5187fa0ff495cb70cb734e
Alpar Matyas <alparmat@gmail.com>
2020-01-22 11:44:43 +0200

    Update plotyzplane.m

--- PipeDemoStandalone/plotyzplane.m
+++ PipeDemoStandalone/plotyzplane.m
@@ -4,7 +4,7 @@
 %
 %  Input:  	                                                                                  
 %
-%  Output: Figure showing thez-velocity, pressure and, x-vortycity in the 
+%  Output: Figure showing the z-velocity, pressure and, x-vortycity in the 
 %          yz plane. The values are raw lattice (numerical) outputs and
 %          have to be scaled to the actual incompressible flow problem
 %
