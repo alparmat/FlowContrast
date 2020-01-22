4cc1be1c6693c6d537b83dd554f7975c0635e53d
Alpar Matyas <alparmat@gmail.com>
2020-01-22 11:10:45 +0200

    Update pipe.m

--- PipeDemoStandalone/pipe.m
+++ PipeDemoStandalone/pipe.m
@@ -1,13 +1,13 @@
 close all; clear all;
 % d is the wall region, l is the total lentgth, lx is the pipe diameter
 d=10; l=300+2*d; lx=100;
-% contains the masked domain
+% initialize masked domain
 IO=zeros(lx,lx,l); 
-[lx,ly,lz]=size(IO); ox=(lx-1)/2; oy=(ly-1)/2; r=ox;
+[lx,ly,lz]=size(IO); ox=(lx)/2; oy=(ly)/2; r=ox;
 
 % calculate the pipe domain
 for i=1:lx, for j=1:ly, for k=1:lz
-    IO(i,j,k)=((+(i-ox-1)^2+(j-oy-1)^2)<r^2)*125;
+    IO(i,j,k)=((+(i-ox)^2+(j-oy)^2)<=r^2)*125;
 end, end, end
 IO=(~IO)*125;
 
@@ -22,7 +22,15 @@ IO2=ones(lx+2*d,lx+2*d,l)*125;
 % calculate final domain
 IO2(d+1:end-d,d+1:end-d,:)=IO; IO=IO2;
 clear IO1; clear IO2;
-
 [lx,ly,lz]=size(IO);
 % write pipe domain text file
 IO=uint8(IO); fid = fopen('pipe.txt', 'wt'); fprintf(fid, '%d\n', IO); fclose(fid)
+% generate numerical parameters file that contains the blend factor,
+% numerical p-difference, numerical viscosity, coarse corrective stencil
+% length, statistical average switch, and blend factor for the correction
+% order
+fid = fopen('param.txt', 'wt'); 
+fprintf(fid, '%f\n%f\n%f\n%d\n%d\n', 1.0,0.003,40.06e-4,2,1,0.0); 
+fclose(fid)
+
+
